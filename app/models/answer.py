b2fabf5869a9293ac63299019be08632e7af66e9
Asliddin Abdumannonov <asliddinabdumannonov06@gmail.com>
2025-04-19 00:34:07 +0500

    modify answers model

--- app/models/answer.py
+++ app/models/answer.py
@@ -1,6 +1,6 @@
 # app/models/answer.py
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Integer, ForeignKey, DateTime, Boolean
+from sqlalchemy import Integer, Boolean, DateTime, ForeignKey
 from datetime import datetime
 from app.core.database import Base
 
@@ -8,13 +8,13 @@ class Answer(Base):
     __tablename__ = "answers"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    game_id: Mapped[int] = mapped_column(ForeignKey("games.id"))
     question_id: Mapped[int] = mapped_column(ForeignKey("questions.id"))
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    game_id: Mapped[int] = mapped_column(ForeignKey("games.id"))
     selected_option_id: Mapped[int] = mapped_column(ForeignKey("options.id"))
-    is_correct: Mapped[bool] = mapped_column()
+    is_correct: Mapped[bool]
     answered_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
 
+    user = relationship("User", back_populates="answers")
     game = relationship("Game", back_populates="answers")
     question = relationship("Question", back_populates="answers")
-    user = relationship("User", back_populates="answers")
